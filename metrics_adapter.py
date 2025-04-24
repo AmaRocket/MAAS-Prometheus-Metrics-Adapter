fe211f7fb3089e1a619c9bed6ecfb56ecbab982a
AmaRocket <alexeverdie999@gmail.com>
2025-04-24 18:40:10 +0200

    cosmetic fix

--- metrics_adapter.py
+++ metrics_adapter.py
@@ -1,15 +1,17 @@
 import os
 
-from flask import Flask, Response
 import requests
+from flask import Flask, Response
 
 app = Flask(__name__)
 METRICS_URL = os.getenv("METRICS_URL")
 
-@app.route('/metrics')
+
+@app.route("/metrics")
 def proxy_maas_metrics():
     resp = requests.get(METRICS_URL)
-    return Response(resp.text, mimetype='text/plain')
+    return Response(resp.text, mimetype="text/plain")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001)
